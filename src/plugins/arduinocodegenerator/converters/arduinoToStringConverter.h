881a8e718e043763f178908990d0eff5aa1dd7b2
Anton Pronin <gorka19800@gmail.com>
2022-10-24 21:19:47 +0400

    Additional wrong commit

--- src/plugins/arduinocodegenerator/converters/arduinoToStringConverter.h
+++ src/plugins/arduinocodegenerator/converters/arduinoToStringConverter.h
@@ -97,7 +97,7 @@ namespace Arduino{
                 result.append("--");
                 break;
             case INC:
-                result.append("ps");
+                result.append("psasd");
                 break;
         }
 

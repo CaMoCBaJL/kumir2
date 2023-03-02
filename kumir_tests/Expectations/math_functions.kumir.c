7e10d6deae2ce7b4a8ef5b8aaa05cd2e6b7a36f0
Anton Pronin <gorka19800@gmail.com>
2023-03-02 21:57:52 +0400

    Added tests for math functions

---
+++ kumir_tests/Expectations/math_functions.kumir.c
@@ -0,0 +1,45 @@
+// license
+// generated from kumir version 2 1 0
+#include <cmath>
+
+void main()
+{
+float а;
+а = sqrt(2);
+float а1;
+а1 = abs(2 - 5);
+float а2;
+а2 = fabs(2 - 5);
+float а3;
+а3 = 11 % 5;
+float а4;
+а4 = 11 / 5;
+float а5;
+а5 = sqrt(2);
+float а6;
+а6 = pow(-1, signbit(-1));
+float а7;
+а7 = sin(60);
+float а8;
+а8 = cos(60);
+float а9;
+а9 = tg(60);
+float а10;
+а10 = ctg(60);
+float а11;
+а11 = log(2);
+float а12;
+а12 = log10(2);
+float а13;
+а13 = fmin(2, 5);
+float а14;
+а14 = fmax(2, 5);
+float а15;
+а15 = exp(2);
+float а16;
+а16 = std::rand(2);
+int а17;
+а17 = round(2.5);
+return ;
+}
+

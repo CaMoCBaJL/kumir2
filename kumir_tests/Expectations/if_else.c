3e875528819d202508b8d3db1956504727849f74
Anton Pronin <gorka19800@gmail.com>
2023-03-03 22:27:33 +0400

    Added tests for conditional functions

---
+++ kumir_tests/Expectations/if_else.c
@@ -0,0 +1,102 @@
+// license
+// generated from kumir version 2 1 0
+
+void main()
+{
+if(true){
+}
+
+if(true){
+else {
+}
+}
+
+if(true){
+if(false){
+}
+}
+
+if(true){
+if(false){
+else {
+}
+}
+}
+
+if(true){
+if(false){
+}
+else {
+if(true){
+}
+}
+}
+
+if(true){
+else if(true){
+}
+}
+
+int а;
+int а1;
+int а2;
+int а3;
+int а4;
+int а5;
+int а6;
+int а7;
+int а8;
+int а9;
+int а10;
+int а11;
+int а12;
+int а13;
+if(true){
+а = 4;
+}
+
+if(true){
+а1 = 1;
+else {
+а2 = -1;
+}
+
+if(true){
+а3 = 4;
+if(false){
+а4 = 41;
+}
+}
+
+if(true){
+а5 = 4;
+if(false){
+а6 = 6;
+else {
+а7 = 4123;
+}
+}
+}
+
+if(true){
+а8 = 4;
+if(false){
+а9 = 42123;
+}
+else {
+а10 = 4;
+if(true){
+а11 = 4;
+}
+}
+}
+
+if(true){
+а12 = 4;
+else if(true){
+а13 = 4;
+}
+}
+return ;
+}
+

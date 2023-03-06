18da56153ec42d258bbd738548571a606e73df6f
Anton Pronin <gorka19800@gmail.com>
2023-03-07 00:06:43 +0400

    Added tests for cycles

---
+++ kumir_tests/Tests/Cycles/For/for.kumir.c
@@ -0,0 +1,50 @@
+// license
+// generated from kumir version 2 1 0
+
+void main()
+{
+int б;
+int а = 14;
+for (int loopIterator0 = 0;loopIterator0 < 1;loopIterator0++;)
+{
+
+}
+for (int loopIterator1 = 0;loopIterator1 < 120;loopIterator1++;)
+{
+
+}for (int б = 1;int б < 9;int бfor (int б = int а;int б > 9;int бfor (int б = 1;int б < 9;int бfor (int б = int а;int б > 9;int бfor (int б = 14;int б >= int а;int бfor (int б = 100;int б > int а;int бfor (int б = 100;int б > 1;int б++)
+{
+
+}
+for (int loopIterator2 = 0;loopIterator2 < 1;loopIterator2++;)
+{
+б = б + 1;
+
+}
+for (int loopIterator3 = 0;loopIterator3 < 120;loopIterator3++;)
+{
+б = б + 1;
+
+}
+)
+{
+б = б + 1for (int б = 1;int б < 9;int б++)
+{
+б = б + 1for (int б = int а;int б > 9;int б++)
+{
+б = б - 1for (int б = 1;int б < 9;int б++)
+{
+б = б - 1for (int б = int а;int б > 9;int б + )
+{
+б = б + 1for (int б = 14;int б >= int а;int б++)
+{
+б = б + 1for (int б = 100;int б > int а;int б++)
+{
+б = б + 1for (int б = 100;int б > 1;int б++)
+{
+б = б + 1;
+
+}
+return ;
+}
+
